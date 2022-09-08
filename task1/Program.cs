c55df44cf79c76d3b27d9a46b2b1dbe6f8ebcf6d
Nikita Smetannikov <smetannikovnikita@yandex.ru>
2022-09-08 16:56:52 +0300

    Task resolved

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите числа через пробел:");
+string strNumbers = Console.ReadLine();
+
+string[] numbers = strNumbers.Split(' ');
+
+int count = 0;
+for (int i = 0; i < numbers.Length; i++)
+{
+    string convertedChar = Convert.ToString(numbers[i]);
+    if (Convert.ToDouble(convertedChar) > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Чисел больше нуля => {count}");
